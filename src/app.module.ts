b21c350d10ff3464f6d54b836b3d76475157696c
Mohammad hassan daneshvar <daneshvar.email@gmail.com>
2021-09-17 18:51:20 +0300

    feat(common): create a module for handling all db connections

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,14 +5,19 @@ import { ConfigModule } from '@nestjs/config';
 import { MessageBrokerModule } from './common/message-broker/messageBroker.module';
 import { ExceptionModule } from './common/exception/exception.module';
 import { ResponseDecoratorModule } from './common/response-decorator/responseDecorator.module';
+import { DatabaseModule } from './common/database/database.module';
+import { Article } from './article.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
+    TypeOrmModule.forFeature([Article]),
     MessageBrokerModule,
     ExceptionModule.register(),
     ResponseDecoratorModule.register(),
     ConfigModule.forRoot(),
     CacheModule.register(),
+    DatabaseModule.register('redis'),
   ],
   controllers: [AppController],
   providers: [AppService],
