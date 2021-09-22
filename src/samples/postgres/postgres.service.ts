570570c1d99f9fc059090b99c0cca5f3f606f357
Mohammad hassan daneshvar <daneshvar.email@gmail.com>
2021-09-22 14:21:59 +0300

    refactor(samples): change the strategy for catching not-found-exception in finder methods

--- src/samples/postgres/postgres.service.ts
+++ src/samples/postgres/postgres.service.ts
@@ -6,24 +6,46 @@ import { AddProductDto } from './dto/add-product.dto';
 
 @Injectable()
 export class PostgresService {
+  /**
+   *
+   * @param productRepository
+   */
   constructor(
     @InjectRepository(Product) private productRepository: Repository<Product>,
   ) {}
 
+  /**
+   *
+   */
   public async find(): Promise<Product[]> {
     return this.productRepository.find();
   }
 
+  /**
+   *
+   * @param id
+   */
   public async findById(id: string): Promise<Product> {
-    return await this.productRepository.findOneOrFail(id).catch(() => {
+    try {
+      return await this.productRepository.findOneOrFail(id);
+    } catch (e) {
       throw new NotFoundException('Product not found!');
-    });
+    }
   }
 
+  /**
+   *
+   * @param newProduct
+   */
   public async addNewProduct(newProduct: AddProductDto): Promise<Product> {
     return await this.productRepository.save(newProduct.toEntity());
   }
 
+  /**
+   *
+   * @param productId
+   * @param updateProduct
+   */
   public async updateProduct(
     productId: string,
     updateProduct: AddProductDto,
@@ -36,6 +58,10 @@ export class PostgresService {
     return await this.productRepository.save(product);
   }
 
+  /**
+   *
+   * @param productId
+   */
   public async deleteProduct(productId: string): Promise<void> {
     const product = await this.findById(productId);
     await this.productRepository.delete(product);
