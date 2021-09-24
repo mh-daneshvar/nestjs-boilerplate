d6c53f036b3c177eb2b7e1d2fa5123fa0dfde460
Mohammad hassan daneshvar <daneshvar.email@gmail.com>
2021-09-24 18:17:24 +0300

    refactor(rabbitmq): #WIP refactor the process of subscribing and publishing from/to a queue

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,6 +1,5 @@
 import { CACHE_MANAGER, Inject, Injectable } from '@nestjs/common';
 import { Cache } from 'cache-manager';
-import Queues from './common/message-broker/strategies/rabbitmq/constants/queues';
 import MessageBrokerInterface from './common/message-broker/MessageBroker.interface';
 
 @Injectable()
@@ -12,9 +11,11 @@ export class AppService {
 
   async getHello(): Promise<any> {
     const salam = Date.now();
-    this.messageBroker.publish(Queues.publishing.login, {
+
+    this.messageBroker.publish('boilerplate.user.add.v1', {
       key: 'salam be rooye mahet ' + salam,
     });
+
     if (!salam) {
       await this.cacheManager.set('key', 'fuck this world');
       return 'Hello World!';
