c55368f29d63dfb7d961fed436df0912de748184
mohammad hassan daneshvar <m.daneshvar@alibaba.ir>
2021-10-19 20:30:10 +0330

    feat(docker-compose): update script.sh file for making development easier

--- scripts.sh
+++ scripts.sh
@@ -1,22 +1,46 @@
 #! /bin/bash
 
-# create a function for running all containers
-function startAllContainers() {
-    docker start $(docker ps -aq --filter "name=nestjs_boilerplate_")
-}
-# create a function for stopping all container
-function stopAllContainers() {
-    docker start $(docker ps -aq --filter "name=nestjs_boilerplate_")
+# --------------------------------------------
+# Functions
+# --------------------------------------------
+# Setup docker-compose file
+function setupDockerCompose() {
+    docker-compose -f docker-compose.local.yaml up --build
 }
 
-# create a function for checking containers statuses
+# Start all containers
+function startAllContainers() {
+    docker start $(docker ps -aq --filter "name=osiris_mono_")
+}
+
+# Stop all containers
+function stopAllContainers() {
+    docker start $(docker ps -aq --filter "name=osiris_mono_")
+}
+
+# Remove and clear all containers
+function removeAllContainers() {
+    read -p "Are you sure? " -n 1 -r
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        docker-compose -f docker-compose.local.yaml down -v
+    fi
+}
+
+# Check status of all containers
 function checkStatus() {
     docker ps -a
 }
 
-read -rep $'\033[32m What do you need? [run|stop|doctor]:\033[34m ' command
+# --------------------------------------------
+# Main
+# --------------------------------------------
+read -rep $'\033[32m What do you need? [setup | run | stop | doctor | removeAll]:\033[36m ' command
 
 case $command in
+  setup)
+    setupDockerCompose
+    ;;
   run)
     startAllContainers
     ;;
@@ -25,6 +49,9 @@ case $command in
     ;;
   doctor)
     checkStatus
+    ;;
+  removeAll)
+    removeAllContainers
     ;;
   *)
     echo $'\033[31m Wrong Command! You are tired, go an make a cup of tea :) \033[0m'
